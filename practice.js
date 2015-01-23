929a39558a8b38aa6a9fe6ad2e3811976ec743c4
Stephen Done <srdone@gmail.com>
2015-01-22 19:19:01 -0700

    complete

--- practice.js
+++ practice.js
@@ -4,17 +4,18 @@ var outer = function(){
   var name = 'Tyler';
   return function(){
     return 'The original name was ' + name;
-  }
-}
+  };
+};
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
   //Code Here
+  var inner = outer();
 
 //Once you do that, invoke inner.
 
   //Code Here
-
+  inner();
 
 
 //Next problem
@@ -33,8 +34,8 @@ var callFriend = function(){
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //Code Here
-
-
+  var callJake = callFriend();
+  console.log(callJake("435-215-9248"));
 
 //Next Problem
 
@@ -45,11 +46,18 @@ var callFriend = function(){
 */
 
   //Code Here
+  var makeCounter = function() {
+    var counter = 0;
+    return function() {
+      console.log(++counter);
+    };
+  };
+
   var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -64,6 +72,27 @@ var callFriend = function(){
   After the function has been called N number of times, console.log('STAHHP');
 */
 
+var limitFunction = function(cb, n) {
+  // A default value for n - num = 0 when n is falsy
+  var num = n || 1;
+  return function() {
+    if (num > 0) {
+      // we want to allow all of the normal parameters to be passed to the function
+      // so we call it using apply instead of the normal ()
+      cb.apply(undefined, arguments);
+      num--;
+    } else {
+      console.log('STAHHP');
+    }
+  };
+};
 
+var limitedConsolelogOnce = limitFunction(console.log);
+limitedConsolelogOnce("kill, kill, kill","ha");
+limitedConsolelogOnce("oh no!");
 
-
+var limitedConsoleLogTwice = limitFunction(console.log, 2);
+limitedConsoleLogTwice("a", "x");
+limitedConsoleLogTwice("b", "x");
+limitedConsoleLogTwice("c", "x");
+limitedConsoleLogTwice("c", "x");
